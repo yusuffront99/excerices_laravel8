e7d27279142730e0aeb5bd097b81bfa17027d07f
yusuf <yusuffront@gmail.com>
2021-09-04 15:16:51 +0800

    update file posts

--- lesson1/resources/views/posts.blade.php
+++ lesson1/resources/views/posts.blade.php
@@ -6,10 +6,10 @@
         <div class="col-sm-6 mb-3">
             <div class="card">
             <div class="card-body">
-                <h5 class="card-title"> {{$post['judul']}} </h5>
-                <h5 class="card-subtitle text-muted"> {{$post['author']}} </h5><hr>
-                <p class="card-text mt-3"> {{$post['body']}} </p>
-                <a href="/posts/{{$post['slug']}}" class="btn btn-primary">Go somewhere</a>
+                <h5 class="card-title"> {{$post->title}} </h5>
+                <h5 class="card-subtitle text-muted"> {{$post->judul}} </h5><hr>
+                <p class="card-text mt-3"> {{$post->body}} </p>
+                <a href="/posts/{{$post->id}}" class="btn btn-primary">Go somewhere</a>
             </div>
             </div>
         </div>

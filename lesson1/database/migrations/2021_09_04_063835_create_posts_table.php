7522b816e6fa4bb9a29ad931c27c40cba8ae6e8c
yusuf <yusuffront@gmail.com>
2021-09-05 11:21:06 +0800

    add files in file category and category database

--- lesson1/database/migrations/2021_09_04_063835_create_posts_table.php
+++ lesson1/database/migrations/2021_09_04_063835_create_posts_table.php
@@ -16,6 +16,7 @@ class CreatePostsTable extends Migration
         Schema::create('posts', function (Blueprint $table) {
             $table->id();
             $table->string('title');
+            $table->foreignId('category_id');
             $table->string('slug')->unique();
             $table->text('excerpt');
             $table->text('body');

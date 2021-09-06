2b5e8423a2b14474c0e0e5ce6f0a1d784d8fa09a
yusuf <yusuffront@gmail.com>
2021-09-07 05:56:56 +0800

    create new file and folders

---
+++ lesson2/resources/views/navs/navbar.blade.php
@@ -0,0 +1,16 @@
+    <div class="container mt-2">
+        <ul class="nav nav-tabs bg-dark px-2 py-2">
+            <li class="nav-item">
+                <a class="nav-link active" aria-current="page" href="#">Home</a>
+            </li>
+            <li class="nav-item">
+                <a class="nav-link" href="#">About</a>
+            </li>
+            <li class="nav-item">
+                <a class="nav-link" href="#">My Blogs</a>
+            </li>
+            <li class="nav-item">
+                <a class="nav-link disabled" href="#" tabindex="-1" aria-disabled="true">Disabled</a>
+            </li>
+        </ul>
+    </div>

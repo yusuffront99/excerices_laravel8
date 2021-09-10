e22d81c896fddcb787b975036a49159a1c0f12d2
yusuf <yusuffront@gmail.com>
2021-09-10 14:30:17 +0800

    update fill file

---
+++ exercise_2/app/Http/Controllers/CategoryController.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use App\Models\Category;
+
+class CategoryController extends Controller
+{
+    //
+    public function categories(Category $category)
+    {
+        return view('category', [
+            "posts"=>$category
+        ]);
+    }
+}
--- exercise_2/routes/web.php
+++ exercise_2/routes/web.php
@@ -1,9 +1,10 @@
 <?php
 
-use Illuminate\Support\Facades\Route;
-use App\Http\Controllers\PostController;
 use App\Models\Post;
 use App\Models\Category;
+use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\PostController;
+use App\Http\Controllers\CategoryController;
 
 
 /*
@@ -29,13 +30,7 @@ Route::get('/posts', [PostController::class, 'index']);
 
 Route::get('/posts/{post:title}', [PostController::class, 'details']);
 
-Route::get('/categories/{category:slug}', function(Category $category){
-    return view('category', [
-        "title"=>$category->name,
-        "posts"=>$category->posts,
-        "category"=>$category->name
-    ]);
-});
+Route::get('/categories/{category:slug}', [CategoryController::class, 'categories']);
 
 
 

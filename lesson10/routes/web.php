b8161607811bf84a40ecb21b2541783ffb66cc80
yusuf <yusuffront@gmail.com>
2021-09-13 08:59:03 +0800

    lesson relations table

--- lesson10/routes/web.php
+++ lesson10/routes/web.php
@@ -14,5 +14,7 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-    return view('welcome');
+    return view('posts');
 });
+
+Route::get('/posts', [PostController::class, 'index']);

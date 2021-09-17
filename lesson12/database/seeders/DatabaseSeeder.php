0e78e77d7086cf43f1c96d66390dae747c7ef59a
yusuf <yusuffront@gmail.com>
2021-09-18 05:15:11 +0800

    create new file in database

--- lesson12/database/seeders/DatabaseSeeder.php
+++ lesson12/database/seeders/DatabaseSeeder.php
@@ -3,6 +3,8 @@
 namespace Database\Seeders;
 
 use Illuminate\Database\Seeder;
+use App\Models\User;
+use App\Models\Post;
 
 class DatabaseSeeder extends Seeder
 {
@@ -13,6 +15,9 @@ class DatabaseSeeder extends Seeder
      */
     public function run()
     {
-        // \App\Models\User::factory(10)->create();
+        User::factory(10)->create();
+
+        Post::factory(3)->create();
+        
     }
 }

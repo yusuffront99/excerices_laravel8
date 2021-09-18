2679c553006c2017d4c66785f7f1382d63876130
yusuf <yusuffront@gmail.com>
2021-09-18 21:24:51 +0800

    update file content posts

--- lesson12/resources/views/posts.blade.php
+++ lesson12/resources/views/posts.blade.php
@@ -25,14 +25,16 @@
 @section('container')
     <div class="container mt-4">
         <h3>{!!$title!!}</h3>
+        <small class="text-muted mb-2">&#9201; lasted post {{$posts[0]->created_at->diffForHumans()}}</small>
 
         @if ($posts->count())
-        <div class="card mb-3">
-            <img src="..." class="card-img-top" alt="...">
-            <div class="card-body">
-                <h5 class="card-title">Card title</h5>
-                <p class="card-text">This is a wider card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
-                <p class="card-text"><small class="text-muted">Last updated 3 mins ago</small></p>
+        <div class="card mb-3 mt-4">
+            <img src="https://source.unsplash.com/1600x400?{{$posts[0]->category->name}}" class="card-img-top" alt="...">
+            <div class="card-body text-center">
+                <h5 class="card-title">{{$posts[0]->title}}</h5>
+                <p class="card-text">{{$posts[0]->summary}}</p>
+                <p class="card-text"><small>Wrote by : <a href="/authors/{{$posts[0]->author->username}}" class="text-decoration-none">{{$posts[0]->author->name}} </a> in <a href="/categories/{{$posts[0]->category->slug}}" class="text-decoration-none">{{$posts[0]->category->name}}</a> </small></p>
+                <a class="btn btn-primary btn-sm" href="/posts/{{$posts[0]->slug}}">Read More</a>
             </div>
         </div>
         @else

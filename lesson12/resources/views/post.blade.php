23eeef0bec97ba9deef5bab4c7eba67efd632e0c
yusuf <yusuffront@gmail.com>
2021-09-20 07:10:34 +0800

    update file content single post

--- lesson12/resources/views/post.blade.php
+++ lesson12/resources/views/post.blade.php
@@ -2,21 +2,17 @@
 
 @section('container')
     <div class="container mt-4">
-        <div class="card">
-            <div class="card-header">
-                Featured
-            </div>
+
+            <p class="card-text"><small>Wrote by : <a href="/authors/{{$post->author->username}}" class="text-decoration-none">{{$post->author->name}}</a> &#9201; {{$post->created_at->diffForHumans()}}</small></p>
+
+            <img src="https://source.unsplash.com/1600x400?{{$post->category->name}}" class="card-img-top" alt="{{$post->category->name}}">
             <div class="card-body">
                 <h3 class="card-title text-center">{{$post->title}}</h3><hr>
-                <p>
-                    <b>By Created : <a href="/authors/{{$post->author->username}}" class="text-decoration-none">{{$post->author->name}}</a> in <a href="/categories/{{$post->category->slug}}" class="text-decoration-none">{{$post->category->name}}</a> </b>
-                </p>
                 <p class="card-text">{!!$post->content!!}</p>
-                <a href="/posts" class="btn btn-primary">Go somewhere</a>
+                <a href="/posts" class="btn btn-primary">Back to Main</a>
             </div>
-            <div class="card-footer text-muted">
+            {{-- <div class="card-footer text-muted">
                 2 days ago
-            </div>
-        </div>
+            </div> --}}
     </div>
 @endsection

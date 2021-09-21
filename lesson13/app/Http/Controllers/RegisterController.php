8337f156be7f0b3a194d43eb9b541772120ece1a
yusuf <yusuffront@gmail.com>
2021-09-21 20:25:59 +0800

    insert data to databases

--- lesson13/app/Http/Controllers/RegisterController.php
+++ lesson13/app/Http/Controllers/RegisterController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\Models\User;
 
 class RegisterController extends Controller
 {
@@ -17,13 +18,14 @@ class RegisterController extends Controller
     public function store(Request $request)
     {
         // get data request
-        $request->validate([
-        'name' => 'required|max:255',
-        'username' => ['required', 'min:3', 'max:255', 'unique:users'],
-        'email' => 'required|email:dns|unique:users',
-        'password' => 'required|min:5|max:255'
-        ]);
-
-        dd('success');
+        $validateData =  $request->validate([
+            'name' => 'required|max:255',
+            'username' => ['required', 'min:3', 'max:255', 'unique:users'],
+            'email' => 'required|email:dns|unique:users',
+            'password' => 'required|min:5|max:255'
+            ]);
+    
+        // dd('success');
+        User::create($validateData);
     }
 }

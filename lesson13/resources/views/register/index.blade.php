49a42adb6ea6c6670b27cc1573398188faaba682
yusuf <yusuffront@gmail.com>
2021-09-21 20:19:25 +0800

    update file content of verification register

--- lesson13/resources/views/register/index.blade.php
+++ lesson13/resources/views/register/index.blade.php
@@ -8,23 +8,43 @@
             <form cation="/register" method="post">
                 @csrf
                 <div class="form-floating">
-                    <input type="text" name="name" class="form-control rounded-top" id="name" placeholder="Name">
+                    <input type="text" name="name" class="form-control rounded-top  @error('name') is-invalid @enderror" id="name" placeholder="Name" value="{{old('name')}}" required>
                     <label for="name">Name</label>
+                    @error('name')
+                    <div class="invalid-feedback">
+                        {{$message}}
+                    </div>
+                    @enderror
                 </div>
 
                 <div class="form-floating">
-                    <input type="text" name="username" class="form-control" id="username" placeholder="Username">
+                    <input type="text" name="username" class="form-control  @error('username') is-invalid @enderror" id="username" placeholder="Username" value="{{old('username')}}"  required>
                     <label for="username">Username</label>
+                    @error('username')
+                    <div class="invalid-feedback">
+                        {{$message}}
+                    </div>
+                    @enderror
                 </div>
 
                 <div class="form-floating">
-                    <input type="email" name="email" class="form-control" id="email" placeholder="Email">
+                    <input type="email" name="email" class="form-control  @error('email') is-invalid @enderror" id="email" placeholder="Email" value="{{old('email')}}" required>
                     <label for="email">Email address</label>
+                    @error('email')
+                    <div class="invalid-feedback">
+                        {{$message}}
+                    </div>
+                    @enderror
                 </div>
 
                 <div class="form-floating">
-                    <input type="password" name="password" class="form-control rounded-bottom" id="password" placeholder="Password">
+                    <input type="password" name="password" class="form-control rounded-bottom  @error('password') is-invalid @enderror" id="password" placeholder="Password" required>
                     <label for="password">Email Password</label>
+                    @error('password')
+                    <div class="invalid-feedback">
+                        {{$message}}
+                    </div>
+                    @enderror
                 </div>
 
                 <button class="w-100 btn btn-lg btn-primary mt-4" type="submit">Register</button>

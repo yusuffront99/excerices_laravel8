a9487f9757005f81aaf5a863b83d3156fe761d8d
yusuf <yusuffront@gmail.com>
2021-09-24 16:17:45 +0800

    update file content --validations message

--- lesson13_1/resources/views/login/index.blade.php
+++ lesson13_1/resources/views/login/index.blade.php
@@ -5,22 +5,27 @@
 <div class="row justify-content-center">
     <div class="col-md-4">
         <h1 class="text-center mt-4">Form Login</h1>
-        @if(session()->has('success'))
-                    <div class="alert alert-success alert-dismissible fade show" role="alert">
-                        <strong>{{session('success')}}</strong>
-                        <button type="button" class="btn-close" data-bs-dismiss="alert" aria-label="Close"></button>
-                    </div>
-                @endif
         <main class="form-signin">
+        @if(session()->has('success'))
+            <div class="alert alert-success alert-dismissible fade show" role="alert">
+            <strong>{{session('success')}}</strong>
+            <button type="button" class="btn-close" data-bs-dismiss="alert" aria-label="Close"></button>
+            </div>
+        @endif
             <form action="" method="POST">
                 @csrf
                 <div class="form-floating">
-                <input type="email" class="form-control" id="email" placeholder="name@example.com" name="email" autofocus required>
-                <label for="email">Email address</label>
+                    <input type="email" class="form-control @error('email') is-invalid @enderror" id="email" placeholder="name@example.com" name="email" value="{{old('email')}}" autofocus required>
+                    <label for="email">Email address</label>
+                    @error('email')
+                    <div class="invalid-feedback">
+                        {{$message}}
+                    </div>
+                    @enderror 
                 </div>
                 <div class="form-floating">
-                <input type="password" class="form-control" id="password" name="password" placeholder="Password">
-                <label for="password">Password</label>
+                    <input type="password" class="form-control" id="password" name="password" placeholder="Password">
+                    <label for="password">Password</label>
                 </div>
         
                 <button class="w-100 btn btn-lg btn-primary" type="submit">Sign in</button>

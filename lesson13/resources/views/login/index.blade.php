81ccdaa4ad9727e4d24dd062624beffd0c614f1d
yusuf <yusuffront@gmail.com>
2021-09-25 07:43:02 +0800

    update file content lesson13

--- lesson13/resources/views/login/index.blade.php
+++ lesson13/resources/views/login/index.blade.php
@@ -4,25 +4,41 @@
 <div class="row justify-content-center mt-5">
     <div class="col-md-4">
 
-        @if (session()->has('success'))
-            
-        <div class="alert alert-success alert-dismissable fade show" role="alert">
-            {{session('success')}}
-            <button class="btn-close" type="button" data-bs-dismiss="alert" aria-label="Close"></button>
-        </div>
+        @if (session()->has('success'))     
+            <div class="alert alert-success alert-dismissable fade show" role="alert">
+                {{session('success')}}
+                <button class="btn-close" type="button" data-bs-dismiss="alert" aria-label="Close"></button>
+            </div>
+        @endif
 
+        @if (session()->has('failed'))
+            <div class="alert alert-success alert-dismissable fade show" role="alert">
+                {{session('failed')}}
+                <button class="btn-close" type="button" data-bs-dismiss="alert" aria-label="Close"></button>
+            </div>
         @endif
 
         <main class="form-signin">
             <h1 class="h3 mb-3 fw-normal text-center">Please Login</h1>
-            <form>
+            <form action="" method="POST">
+                @csrf
                 <div class="form-floating">
-                    <input type="email" class="form-control" id="floatingInput" placeholder="name@example.com">
-                    <label for="floatingInput">Email address</label>
+                    <input type="email" class="form-control @error('email') is-invalid @enderror" id="email" placeholder="email@example.com" name="email">
+                    @error('email')
+                        <div class="invalid-feedback">
+                            {{$message}}
+                        </div>
+                    @enderror
+                    <label for="email">Email address</label>
                 </div>
                 <div class="form-floating">
-                    <input type="password" class="form-control" id="floatingPassword" placeholder="Password">
-                    <label for="floatingPassword">Password</label>
+                    <input type="password" class="form-control @error('password') is-invalid @enderror" id="password" placeholder="Password" name="password">
+                    @error('password')
+                    <div class="invalid-feedback">
+                        {{$message}}
+                    </div>
+                    @enderror
+                    <label for="password">Password</label>
                 </div>
                 <button class="w-100 btn btn-lg btn-primary" type="submit">Login</button>
             </form>
--- lesson13/routes/web.php
+++ lesson13/routes/web.php
@@ -55,6 +55,7 @@ Route::get('/categories', function(){
 
 // laravel part 14
 Route::get('/login', [LoginController::class, 'index']);
+Route::post('/login', [LoginController::class, 'authenticate']);
 
 Route::get('/register', [RegisterController::class, 'index']);
 Route::post('/register', [RegisterController::class, 'store']);

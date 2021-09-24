fccdb31add86d27ba5ed209290375bc1c3c43e5c
yusuf <yusuffront@gmail.com>
2021-09-24 17:14:20 +0800

    middleware and update more file content

--- lesson13_1/resources/views/navs/navbar.blade.php
+++ lesson13_1/resources/views/navs/navbar.blade.php
@@ -20,7 +20,12 @@
                     <ul class="dropdown-menu" aria-labelledby="navbarDropdown">
                         <li><a class="dropdown-item" href="/dashboard"><i class="bi bi-speedometer2"></i> My Dashboard</a></li>
                         <li><hr class="dropdown-divider"></li>
-                        <li><a class="dropdown-item" href="#"><i class="bi bi-box-arrow-right"></i> Logout</a></li>
+                        <li>
+                            <form action="/logout" method="post">
+                                @csrf
+                                <button type="submit" class="dropdown-item"><i class="bi bi-box-arrow-right"></i> Logout</button>
+                            </form>
+                        </li>
                     </ul>
                 </li>
         </ul>
